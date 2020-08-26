078fd85179962fcbd3009d87cc80f1ed0e0ee6c8
Gab55 <gabrielgiroud@gmail.com>
2020-08-26 15:06:19 +0200

    Redirection du header-menu OK

--- Madera/Madera/ClientApp/src/app/pages/header-menu/header-menu.page.ts
+++ Madera/Madera/ClientApp/src/app/pages/header-menu/header-menu.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router"
 
 @Component({
   selector: 'app-header-menu',
@@ -7,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderMenuPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  goTo(numMenu) {
+
+    switch (numMenu) {
+      case 1:
+        this.router.navigate(['/recherche-projet'])
+        break;
+      case 2:
+        this.router.navigate(['/nouveau-projet'])
+        break;
+      case 3:
+        this.router.navigate(['/recherche-devis'])
+        break;
+      case 4:
+        this.router.navigate(['/recherche-client'])
+        break;
+      case 5:
+        this.router.navigate(['/nouveau-client'])
+        break;
+      case 6:
+        this.router.navigate(['/parametrage'])
+        break;
+    }
+
+
   }
 
 }
