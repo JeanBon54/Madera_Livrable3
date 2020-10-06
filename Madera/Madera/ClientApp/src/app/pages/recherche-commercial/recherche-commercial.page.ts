d0be26cda1e53be2e675a1d9bbacca710b97f3ed
RobertPastiss <gabrielgiroud@gmail.com>
2020-10-06 22:01:53 +0200

    Centralisation CSS page de recherche OK

--- Madera/Madera/ClientApp/src/app/pages/recherche-commercial/recherche-commercial.page.ts
+++ Madera/Madera/ClientApp/src/app/pages/recherche-commercial/recherche-commercial.page.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-recherche-commercial',
   templateUrl: './recherche-commercial.page.html',
-  styleUrls: ['./recherche-commercial.page.scss'],
+  styleUrls: ['./recherche-commercial.page.scss','./../../app.component.scss'],
 })
 export class RechercheCommercialPage implements OnInit {
 
