d56ed580dd310bc0bc0c58bc4bb20c31340cf3e4
Gab <gabrielgiroud@gmail.com>
2020-10-07 12:50:16 +0200

    Nouveau client ok

--- Madera/Madera/ClientApp/src/app/pages/nouveau-projet/nouveau-projet.page.ts
+++ Madera/Madera/ClientApp/src/app/pages/nouveau-projet/nouveau-projet.page.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-nouveau-projet',
   templateUrl: './nouveau-projet.page.html',
-  styleUrls: ['./nouveau-projet.page.scss'],
+  styleUrls: ['./nouveau-projet.page.scss','./../../app.component.scss'],
 })
 export class NouveauProjetPage implements OnInit {
 
