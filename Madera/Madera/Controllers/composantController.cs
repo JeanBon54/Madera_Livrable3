c3a1df0d7bee150fb9ce4e4fe2b53b5c3ad479d1
Gab <gabrielgiroud@gmail.com>
2020-10-13 19:00:48 +0200

    Ajout Controller composant

---
+++ Madera/Madera/Controllers/composantController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Madera;
+using Madera.Models;
+
+namespace Madera.Controllers
+{
+    public class composantController : Controller
+    {
+        private readonly DefaultContext _context = null;
+
+        public composantController(DefaultContext context)
+        {
+            this._context = context;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult All()
+        {
+            this.ViewBag.Titre = "Liste des utilisateus";
+            List<Composant> composantListe = new List<Composant>();
+
+            var query = from COMPOSANT in _context.Composant
+                        select COMPOSANT;
+            return View(query.ToList());
+
+        }
+    }
+}
