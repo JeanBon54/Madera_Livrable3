c21d8f2142daba17d2dd84ab8720ace31d5ab969
Gab <gabrielgiroud@gmail.com>
2020-11-04 11:20:33 +0100

    Controller Commercial

---
+++ Madera/Madera/Controllers/CommercialsController.cs
@@ -0,0 +1,109 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Madera.Models;
+
+namespace Madera.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommercialsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public CommercialsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Commercials
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Commercial>>> GetCommercials()
+        {
+            return await _context.Commercials.ToListAsync();
+        }
+
+        // GET: api/Commercials/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Commercial>> GetCommercial(int id)
+        {
+            var commercial = await _context.Commercials.FindAsync(id);
+
+            if (commercial == null)
+            {
+                return NotFound();
+            }
+
+            return commercial;
+        }
+
+        // PUT: api/Commercials/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCommercial(int id, Commercial commercial)
+        {
+            if (id != commercial.ID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(commercial).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CommercialExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Commercials
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Commercial>> PostCommercial(Commercial commercial)
+        {
+            _context.Commercials.Add(commercial);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCommercial", new { id = commercial.ID }, commercial);
+        }
+
+        // DELETE: api/Commercials/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Commercial>> DeleteCommercial(int id)
+        {
+            var commercial = await _context.Commercials.FindAsync(id);
+            if (commercial == null)
+            {
+                return NotFound();
+            }
+
+            _context.Commercials.Remove(commercial);
+            await _context.SaveChangesAsync();
+
+            return commercial;
+        }
+
+        private bool CommercialExists(int id)
+        {
+            return _context.Commercials.Any(e => e.ID == id);
+        }
+    }
+}
