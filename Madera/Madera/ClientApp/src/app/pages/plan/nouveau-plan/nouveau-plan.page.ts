ea42cfb1a35302551bf2ac82d137dee1d9d05d44
RobertPastiss <gabrielgiroud@gmail.com>
2020-11-17 21:20:47 +0100

    Première phase

--- Madera/Madera/ClientApp/src/app/pages/plan/nouveau-plan/nouveau-plan.page.ts
+++ Madera/Madera/ClientApp/src/app/pages/plan/nouveau-plan/nouveau-plan.page.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef,Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl,FormArray,Validators } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router'; 
+import { Observable } from 'rxjs';
+
+
+import { ProjetWebService } from './../../../webServices/projet-webservice.service';
+import { Projet } from 'src/app/models/Projet';
+import { Devis } from 'src/app/models/Devis';
+import { Plan } from 'src/app/models/Plan';
+import { DevisService } from './../../../WebServices/devis-webservice.service';
+import { PlanService } from './../../../WebServices/plan-webservice.service';
+
 
 @Component({
   selector: 'app-nouveau-plan',
   templateUrl: './nouveau-plan.page.html',
-  styleUrls: ['./nouveau-plan.page.scss'],
+  styleUrls: ['./nouveau-plan.page.scss', './../../../app.component.scss'],
 })
 export class NouveauPlanPage implements OnInit {
+  form: FormGroup;
+  projet$: Observable<Projet[]>;
+  plans$: Observable<Plan[]>;
+  devis$: Observable<Devis[]>;
+  projetId: number;
 
-  constructor() { }
+  constructor(private pService: ProjetWebService,
+    private plService: PlanService, 
+    private dService: DevisService,
+    private avRoute: ActivatedRoute,
+    private formBuilder: FormBuilder) {
+
+    const idParam = 'id';
+
+    if (this.avRoute.snapshot.params[idParam]) {
+      this.projetId = this.avRoute.snapshot.params[idParam];
+    }
+  }
 
   ngOnInit() {
+   this.loadProjet();
+   this.loadPlans();
+  }
+
+  loadProjet() {
+    this.projet$ = this.pService.getProjets();
+  }
+
+  loadPlans() {
+    this.plans$ = this.plService.getPlans();
   }
 
 }
