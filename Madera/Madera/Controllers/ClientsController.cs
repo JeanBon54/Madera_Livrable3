dd683d3d2fdbc7f03e7e922143c5c3baafd13026
Gab <gabrielgiroud@gmail.com>
2020-12-14 15:56:59 +0100

    Correction affichage

--- Madera/Madera/Controllers/ClientsController.cs
+++ Madera/Madera/Controllers/ClientsController.cs
@@ -98,6 +98,11 @@ namespace Madera.Controllers
             return CreatedAtAction("GetClient", new { id = client.ID }, client);
         }
 
+
+
+
+
+
         // DELETE: api/Clients/5
         [HttpDelete("{id}")]
         public async Task<ActionResult<Client>> DeleteClient(int id)
