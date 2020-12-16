aa0ee320f85cc989e845f66cf6a558d1043d3998
RobertPastiss <gabrielgiroud@gmail.com>
2020-12-16 15:23:11 +0100

    Révision architecture Madera

--- Madera/Madera/Startup.cs
+++ Madera/Madera/Startup.cs
@@ -38,7 +38,7 @@ namespace Madera
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
-                configuration.RootPath = "ClientApp/dist";
+                configuration.RootPath = "../../ClientApp/dist";
             });
         }
 
